fcc4f81cc06c33c407ed5955e435c25dcb60d418
SarasaGunawardhana <it14078842@my.sliit.lk>
2017-05-29 05:10:10 +0530

    Spring Boot Web Application using REST API

---
+++ src/main/java/SGDevelpoment/courseAPI.java
@@ -0,0 +1,14 @@
+package SGDevelpoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseAPI {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(courseAPI.class, args);
+	}
+
+}
